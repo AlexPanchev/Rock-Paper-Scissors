2fe67c5c9c6321e114dcde5520cd42da5437486d
AlexPanchev <78849043+AlexPanchev@users.noreply.github.com>
2025-03-05 10:37:55 +0200

    Update Rock-Paper-Scissors.py

--- Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors.py
@@ -5,46 +5,39 @@ while True:
     rock = 'Rock'
     paper = 'Paper'
     scissors = 'Scissors'
-
     computer_move = ''
     points = 0
 
-    player_move = input('Choose [r]ock, [p]aper) or [s]cissors: ')
+    player_move = input("Choose [r]ock, [p]aper or [s]cissors: ")
 
     if player_move == 'r':
         player_move = rock
-
     elif player_move == 'p':
         player_move = paper
-
     elif player_move == 's':
         player_move = scissors
-
     else:
-        raise SystemExit('Invalid Input. Try again...')
+        raise SystemExit("Invalid Input. Try again...")
 
     computer_random_number = random.randint(1, 3)
+
     if computer_random_number == 1:
         computer_move = rock
-
     elif computer_random_number == 2:
         computer_move = paper
-
     else:
         computer_move = scissors
 
-    print(f'The computer chose: {computer_move}')
+    print(f"The computer chose {computer_move}.")
 
-    if(player_move == rock and computer_move == scissors) or \
+    if (player_move == rock and computer_move == scissors) or \
             (player_move == paper and computer_move == rock) or \
             (player_move == scissors and computer_move == paper):
         points += 3
         print(Fore.GREEN, Back.BLACK + f'You win! You have {points} points.')
-
     elif player_move == computer_move:
         points += 1
         print(Fore.YELLOW, Back.BLACK + f'Draw! You have {points} points.')
-
     else:
         print(Fore.RED, Back.BLACK + f'You lose! You have {points} points.')
 
